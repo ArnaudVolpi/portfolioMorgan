21cf616606f605ed5b42ef66c53b2e72f0b3d761
Astol <arnaudvolp@gmail.com>
2018-12-15 12:15:31 +0100

    home

--- view/nav.php
+++ view/nav.php
@@ -1,1 +1,16 @@
-<p>bonjour je suis une nav</p>
+
+        <div class="menu-icon">
+            <span class="menu-icon__line menu-icon__line-left"></span>
+            <span class="menu-icon__line"></span>
+            <span class="menu-icon__line menu-icon__line-right"></span>
+        </div>
+        
+        <div class="nav2">
+            <div class="nav2__content">
+                <ul class="nav2__list">
+                    <li class="nav2__list-item" >Projects</li>
+                    <li class="nav2__list-item">Gallery</li>                    
+                    <li class="nav2__list-item">About</li>
+                </ul>
+            </div>
+        </div>
--- view/template.php
+++ view/template.php
@@ -1,22 +1,33 @@
 <!DOCTYPE html>
 <html lang="fr">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-<!-- Tes liens css -->
-<link rel="stylesheet" href="../public/css/style.css">
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta http-equiv="X-UA-Compatible" content="ie=edge">
+<link rel="icon" type="image/png" href="public/icon/favicon.png" />
 
-    <title>Document</title>
+<!-- Tes liens css -->
+<link rel="stylesheet" href="public/css/style.css">
+
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/meyer-reset/2.0/reset.min.css">
+
+<link rel="stylesheet" href="public/css/navbar.css">
+
+
+
+<title>Morgan Orabona</title>
 </head>
 <body>
-    <!-- Ta navbar -->
-    <?php require_once('nav.php') ?>
+<!-- Ta navbar -->
+<?php require_once('nav.php') ?>
 
-    <!-- Ton Contenu -->
-   
+<!-- Ton Contenu -->
+<?=$content?>
 
-    <!-- Ton Footer -->
-    <?php require_once('footer.php') ?>
+<!-- Ton Footer -->
+<?php require_once('footer.php') ?>
+
+<!-- JS -->
+<script src="public/js/navbar.js"></script>
 </body>
 </html>
